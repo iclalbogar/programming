5a1bd7f780f3ff9a72d140fa0758d8aa12e21528
iclalbogar <esraiclalbogar@gmail.com>
2021-12-12 11:58:24 +0300

    tek çift kontrolü programı

---
+++ Python/if.py
@@ -0,0 +1,19 @@
+# sayi = int(input("Lütfen bir sayı giriniz"))
+
+
+# if (sayi % 2):
+#     print("Sayı tektir")
+
+# else :
+#     print("Sayı çifttir")
+
+sayi = int(input("lütfen bir sayı giriniz"))
+
+if sayi < 0:
+    print("sayı negatif")
+
+elif sayi == 0:
+    print("sayı sıfır")
+
+else:
+    print("sayı pozitif")
