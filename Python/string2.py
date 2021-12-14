a6cbc50290c34a572bfb0857379cbb9449fc800f
iclalbogar <esraiclalbogar@gmail.com>
2021-12-14 21:43:04 +0300

    #1 added string2.py

---
+++ Python/string2.py
@@ -0,0 +1,17 @@
+
+# isim = input("İsminizi giriniz ")
+
+# soyisim = input("Soyisminizi giriniz ")
+
+# birlestirilmis = isim + " " + soyisim
+
+# if isim == "iclal" :
+#     print(birlestirilmis.replace(isim,"sayın"))
+# else :
+#     print(birlestirilmis)
+
+
+isim = input("isminizi ve soyisminizi girin ")
+ 
+if "iclal" not in isim:
+    print("isminiz iclal değil")
