11aeb34aa278760e585be0c904de4a3efd5df20f
Muzaffer-Irmak-Yavuz <muzafferirmaky@gmail.com>
2021-12-14 21:28:00 +0300

    #2 added string1 example

---
+++ Python/string1.py
@@ -0,0 +1,16 @@
+
+
+# get name from user
+ 
+isim = input("İsminizi giriniz :")
+soyisim = input("Soyisminiz giriniz :")
+
+
+birlestirilmis = isim + " " + soyisim
+
+
+duzeltilmis = birlestirilmis.replace(isim,"Sayın")
+
+
+print(duzeltilmis)
+
